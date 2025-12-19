98f9506a054743bf8bcc9fea3f02b2455fe91445
bala-amypo <bala@amypo.in>
2025-12-19 07:21:47 +0000

    User commit

--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -1,61 +1,61 @@
-package com.example.demo.Entity;
-import jakarta.persistence.*;
+    package com.example.demo.Entity;
+    import jakarta.persistence.*;
 
 
-@Entity
-@Table(name="User",uniqueConstraints={
-    @UniqueConstraint(columnNames="email")
-})
-public class User{
-    public User(){
+    @Entity
+    @Table(name="User",uniqueConstraints={
+        @UniqueConstraint(columnNames="email")
+    })
+    public class User{
+        public User(){
 
-    }
- 
-    public enum Role{
-        User,
-       Admin
-    } 
-    @ID
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private String password;
-    @Enumerated(EnumType.String)
-    private Role role = Role.User;
-
-    public Long getid(){
-        return id;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setPass(String pass){
-        this.password = pass;
-    }
-    public String getpass(){
-        return password;
-    }
-    public void setRole(String role){
-      this.role = role;
-    }
-    public String getRole(){
-        return role;
-    }
-
-
+        }
     
+        public enum Role{
+            User,
+        Admin
+        } 
+        @Id
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private String email;
+        private String password;
+        @Enumerated(EnumType.STRING)
+        private Role role = Role.User;
+
+        public Long getId(){
+            return id;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setEmail(String email){
+            this.email = email;
+        }
+        public String getEmail(){
+            return email;
+        }
+        public void setPass(String pass){
+            this.password = pass;
+        }
+        public String getPass(){
+            return password;
+        }
+        public void setRole(Role role){
+        this.role = role;
+        }
+        public Role getRole(){
+            return role;
+        }
+
+
+        
 
 
 
-}
+    }
