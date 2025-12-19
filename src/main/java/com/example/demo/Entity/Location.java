3eccb9ff905c056cb7df569bd17c6985bccde8b7
bala-amypo <bala@amypo.in>
2025-12-19 09:59:31 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Location.java
+++ src/main/java/com/example/demo/Entity/Location.java
@@ -24,7 +24,18 @@ public class Location{
     public String getName(){
         return name;
     }
-    public void setLatitude(Double latitude)
+    public void setLatitude(Double latitude){
+        this.latitude = latitude;
+    }
+    public Double getLatitude(){
+        return latitude;
+    }
+    public void setLongitude(Double longitude){
+        this.longitude = longitude;
+    }
+    public Double getLongitude(){
+        return longitude;   
+    }
 
 
 
