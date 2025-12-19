5db92e7d5e68a92cc889889b75c407ff7a8bd6b6
bala-amypo <bala@amypo.in>
2025-12-19 13:40:48 +0000

    User commit

--- src/main/java/com/example/demo/Entity/RouterOptimizationResult.java
+++ src/main/java/com/example/demo/Entity/RouterOptimizationResult.java
@@ -2,11 +2,15 @@ package com.example.demo.Entity;
 import jakarta.persistence.*;
 
 @Entity
-@
+@Table(name="RouterOptimiztionResult")
 public class RouterOptimiztionResult{
     public RouterOptimiztionResult(){
-
+        
     }
+
+    @Id
+    @GeneratedValue(strategy=GenrationType.IDENTITY)
+    private lO
 
 
 }
